fd827804222b3bd3d768514ccbaca8b8330d2377
tokorom <tokorom@gmail.com>
2026-05-06 18:02:35 +0900

    First commit

---
+++ PromptFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PromptFlow
+//
+//  Created by Yuta Tokoro on 2026/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PromptFlow/PromptFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptFlowApp.swift
+//  PromptFlow
+//
+//  Created by Yuta Tokoro on 2026/05/06.
+//
+
+import SwiftUI
+
+@main
+struct PromptFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
