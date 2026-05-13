0869247656d8498ed189769cc52c8cb56434c154
tokorom <tokorom@gmail.com>
2026-05-13 12:49:42 +0900

    feat: auto-focus editor after sidebar selection

--- PromptTap/PromptTapModel.swift
+++ PromptTap/PromptTapModel.swift
@@ -190,6 +190,7 @@ final class PromptTapModel: ObservableObject {
                 templateNameBuffer = ""
                 promptText = ""
             }
+            focusEditor()
         }
     }
 
